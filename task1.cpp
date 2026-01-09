43d7e82421bb074bbb5c32c89fc27a617f579a05
adnanali55 <f2025-0968@bnu.edu.pk>
2026-01-08 22:36:07 -0800

    Add average function to calculate mean of three numbers

---
+++ task1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+float average(int a, int b, int c) {
+    return (a + b + c) / 3.0;
+}
+
+
+int main() {
+    cout << average(2, 4, 5) << endl;
+    return 0;
+}
