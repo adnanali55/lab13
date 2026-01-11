0e9df81471f6324be1d36082bfb0ac6e9c03deb7
adnanali55 <f2025-0968@bnu.edu.pk>
2026-01-11 18:38:11 +0500

    Add function to print multiplication tables in range

---
+++ task6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void printTableRange(int start, int end) {
+    for (int i = start; i <= end; i++) {
+        cout << "Table of " << i << endl;
+        for (int j = 1; j <= 10; j++) {
+            cout << i << " x " << j << " = " << i * j << endl;
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int start, end;
+    cout << "Enter start: ";
+    cin >> start;
+    cout << "Enter end: ";
+    cin >> end;
+    printTableRange(start, end);
+    return 0;
+}
