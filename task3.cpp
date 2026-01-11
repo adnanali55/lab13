13d8bfb11571755005f3ba3b79a95e3e7f379e94
adnanali55 <f2025-0968@bnu.edu.pk>
2026-01-11 18:29:18 +0500

    Add task3.cpp to check if a number is even

---
+++ task3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+bool isEven(int n) {
+    return n % 2 == 0;
+}
+
+int main() {
+    int num = 10;
+    if (isEven(num)) {
+        cout << "Even number";
+    } else {
+        cout << "Odd number";
+    }
+    return 0;
+}
